83f50dbe4a5b7159bc9c0cc165c48613673cc6ff
codebykish <kishan_letters@yahoo.com>
2017-10-28 19:42:03 +0200

    Ajax Add and delete working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,46 @@
 class SongsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+
+  def create
+     @artist = Artist.find(params[:artist_id])
+     @song = @artist.songs.new(song_params)
+
+    respond_to do |format|
+      if @song.update_attributes(song_params)
+        format.html { redirect_to songs_path, notice: "Succes!" }
+        format.json { render json: @song, status: 200 }
+      else
+        format.html {render "edit",  notice: "Try again!" }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+
+    @song.destroy
+    respond_to do |format|
+      format.html { redirect_to songs_path, notice: 'Song was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name)
+  end
 end
