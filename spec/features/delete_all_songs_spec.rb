41e0c805a1fef109ec8e4e219b0ef919849518ab
codebykish <kishan_letters@yahoo.com>
2017-10-30 14:16:09 +0100

    Rspec Typo

--- spec/features/delete_all_songs_spec.rb
+++ spec/features/delete_all_songs_spec.rb
@@ -8,19 +8,12 @@ feature 'Manage songs', js: true do
   scenario 'delete all songs' do
 
     visit artist_path(artist)
-    #
-    # fill_in 'Name', with: 'Spiderman'
-    #
-    # click_button("Add Song")
-    #
-    # expect(page).to have_content('Spiderman')
 
-
-    fill_in 'Name', with: 'Hulk'
+    fill_in 'Name', with: 'Spiderman'
 
     click_button("Add Song")
 
-    expect(page).to have_content('Hulk')
+    expect(page).to have_content('Spiderman')
 
 
     click_button("Delete All Songs")
